9f5dd90d4a0f361d30622707de4b5ce64664fe4f
German Monzon <german.monzon.95@gmail.com>
2025-03-12 13:44:51 -0300

    add apiservice

---
+++ frontend/src/utils/apiService.ts
@@ -0,0 +1,71 @@
+import axios, { AxiosInstance, Method, RawAxiosRequestHeaders } from "axios";
+
+interface ApiServiceRequestConfig<BodyType> {
+  method: Method;
+  url: string;
+  headers?: RawAxiosRequestHeaders | undefined;
+  body?: BodyType;
+  params?: any;
+}
+
+interface ApiServiceGetConfig<QueryType> {
+  url: string;
+  query?: QueryType | undefined;
+}
+
+interface ApiServicePostConfig<BodyType> {
+  url: string;
+  body?: BodyType | undefined;
+}
+
+class ApiService {
+  private axiosInstance: AxiosInstance;
+
+  constructor() {
+    this.axiosInstance = axios.create({
+      // @ts-ignore
+      baseURL: import.meta.env.VITE_API_URL,
+    });
+  }
+
+  private async request<D>({
+    method,
+    url,
+    headers = {},
+    body,
+    params
+  }: ApiServiceRequestConfig<D>) {
+    const baseHeaders = {
+      "Content-Type": "application/json"
+    };
+
+    return axios({
+      method,
+      url,
+      headers: {
+        ...baseHeaders,
+        ...headers,
+      },
+      data: body,
+      params,
+    });
+  }
+
+  public async get<Q>({ url, query }: ApiServiceGetConfig<Q>) {
+    return this.request({
+      method: 'GET',
+      url,
+      params: query,
+    });
+  }
+
+  public async post<B>({ url, body }: ApiServicePostConfig<B>) {
+    return this.request({
+      method: 'POST',
+      url,
+      body,
+    });
+  }
+}
+
+export const apiService = new ApiService();
